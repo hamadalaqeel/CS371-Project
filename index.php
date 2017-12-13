c4ffd27f3d6af90742b12372a8382e92915f8510
Alyousfi <32346287+Alyousfi@users.noreply.github.com>
2017-12-13 18:27:47 +0300

    changing conferences to php

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ACL Chapter - Home</title>
+    <meta name="description" content="Free Bootstrap Theme by ProBootstrap.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      
+      <!-- Fixed navbar -->
+      <?php
+      include "navbar.html";
+      ?>
+      <!-- About PSU Section-->
+      <section class="probootstrap-section probootstrap-section-colored">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-left section-heading probootstrap-animate">
+              <h1>About Prince Sultan University</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="probootstrap-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="probootstrap-flex-block">
+                <div class="probootstrap-text probootstrap-animate">
+                  <div class="text-uppercase probootstrap-uppercase">History</div>
+                  <h3>The Leading Non-Profit University in KSA!</h3>
+                  <p>Prince Sultan University (PSU) is a institution of higher education, founded in 1999. The business community of Riyadh first developed the idea of founding the university. They dedicated PSU to HRH Prince Sultan bin Abdul Aziz to celebrate his homecoming after his successful medical treatment aboard. HRH Prince Sultan Abdul Aziz congratulated the community on this initiative, and he gave a generous donation to the Riyadh Philanthropic Society for Sciences to express his personal support for this important undertaking.
+</p>
+                  <p><a href="http://psu.edu.sa/en/pages/default.aspx" class="btn btn-primary btn-primary-learnmore">Learn More</a></p>
+                </div>
+                <div class="probootstrap-image probootstrap-animate" style="background-image: url(img/VideoImg.png)">
+                  <a href="https://www.youtube.com/watch?v=LZWEEwRoVyk" class="btn-video popup-vimeo"><i class="icon-play3"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- Mession and Objectives -->
+      
+      <section class="probootstrap-section">
+        <div class="container">
+          
+          <div class="col-md-6">
+    <h2>Our Mission:</h2>
+            <p>ACM, the Association for Computing Machinery ,is the world's largest educational and scientific society, uniting computing educators, researchers and professionals to inspire dialogue, share resources and address the field's challenges. ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. ACM supports the professional growth of its members by providing opportunities for life-long learning, career development, and professional networking.
+            </p>
+          </div>
+          <div class="col-md-6 col-md-push-1">
+           
+             <h2>Our Objectives: </h2>
+             <ul class="list-group">
+             <li class="list-group-item">Engage students in stimulating computing activities.</li>
+             <li class="list-group-item">Connect students with leaders in the field.</li>
+             <li class="list-group-item">Encourage students to advance the field of computing.</li>
+             <li class="list-group-item">Join mentoring programs for career opportunities.</li>
+             <li class="list-group-item">Broaden the computing community through ACM.</li>  
+             <li class="list-group-item">Network with other ACM Chapter leaders and members.</li>  
+             </ul>
+
+          </div>
+          
+
+        </div>
+      </section>
+      
+      <!--ACM-W-->
+      
+       <section class="probootstrap-section">
+        <div class="container">
+          
+          <div class="probootstrap-animate">
+    <h2>About ACM-W</h2>
+    <p>
+        ACM-W is the ACM committee on Women in Computing. It celebrates, informs and supports women in computing, and works with the ACM-W community of computer scientists, educators, employers and policy makers to improve working and learning environments for women. ACM-W promotes activities that result in more equal representation of women in Computing such as mentoring or role modeling, monitoring the status of women in industrial and academic computing through the gathering of statistics, providing historical information about women's accomplishments and roles in Computing and serving as a repository of information about programs, documents and policies of concern to women in Computing. Through international and local activities and chapters, educational outreach, recognition of leading women in Computing, cooperation with other organizations, and the dissemination of information via newsletters, blogs, and reports, ACM-W promotes the growth and development of women seeking to attain careers in computing.
+
+    </p>
+          </div>
+
+          
+
+        </div>
+      </section>
+      <!-- End of ACM-W-->
+      
+
+
+      <section class="probootstrap-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
+              <h2>Why You Should Join ACM?</h2>
+              <p class="lead">If you think it is important to recruit and retain as many women as possible in the computing field, you should join ACM-W! Together, we will help celebrate, inform and support women in computing:
+</p>
+            </div>
+          </div>
+          <div class="row">
+            <div >
+              <div class="service left-icon probootstrap-animate">
+                <div class="icon"><i class="icon-checkmark"></i></div>
+                <div class="text">
+                  <h3>Celebrate</h3>
+                  <p> 
+                  One of the most popular ways to celebrate women in computing is by starting and/or attending an ACM-W Celebration of Women in Computing; the accomplishments of women in computing are at the heart of ACM-W's e-Newsletter and Blog. And ACM-W encourages its members to celebrate the work of prominent women by nominating them for Awards -- such as the Athena Award & Lecture -- as well as ACM Advanced Grade Membership.
+                  </p>
+                </div>  
+              </div>
+              <div class="service left-icon probootstrap-animate">
+                <div class="icon"><i class="icon-checkmark"></i></div>
+                <div class="text">
+                  <h3>Inform</h3>
+                  <p>
+                Via ACM-W's student scholarship program, young women are informed about the research going on, and become acquainted with the researchers, in their field. Members of ACM-W Student Chapters are informed about the educational and career opportunities available to them.      
+                  </p>
+                </div>
+              </div>
+              <div class="service left-icon probootstrap-animate">
+                <div class="icon"><i class="icon-checkmark"></i></div>
+                <div class="text">
+                  <h3>Support</h3>
+                  <p>
+                 Lend your enthusiasm, inspiration, and support to ACM-W, and help make a difference in the global computing community! 
+                  </p>
+                </div>
+              </div>
+            </div>
+          
+          </div>
+          <!-- END row -->
+        </div>
+      </section>
+      
+     
+      <footer class="probootstrap-footer probootstrap-bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="probootstrap-footer-widget">
+                <h3>About The School</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident suscipit natus a cupiditate ab minus illum quaerat maxime inventore Ea consequatur consectetur hic provident dolor ab aliquam eveniet alias</p>
+                <h3>Social</h3>
+                <ul class="probootstrap-footer-social">
+                  <li><a href="#"><i class="icon-twitter"></i></a></li>
+                  <li><a href="#"><i class="icon-facebook"></i></a></li>
+                  <li><a href="#"><i class="icon-github"></i></a></li>
+                  <li><a href="#"><i class="icon-dribbble"></i></a></li>
+                  <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                  <li><a href="#"><i class="icon-youtube"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <?php 
+                  include 'login.php';
+              ?>
+            <div class="col-md-3 col-md-push-1">
+              <div class="probootstrap-footer-widget">
+                <h3>Links</h3>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Courses</a></li>
+                  <li><a href="#">Teachers</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <?php if (isset($_SESSION["role"])){
+	     echo '<li><a href="logout.php">SIGN OUT</a></li>'; //Add logout.php
+			}else {
+			 echo '<li><a href="login.php">SIGN IN</a></li>';
+		}
+		?>
+							
+		<li><a href="#" class="as-button"><span class="btn btn-primary">SIGN
+										UP</span></a></li>
+	<li><a href ="checkout.php"><i class="fa fa-cart-plus" style="font-size: 20px"></i><?php if (isset($_SESSION["role"]))?></a></li>		
+	<?php
+	if (isset($_SESSION["role"])){
+	 echo '<li><a href="#">Welcome '.$_SESSION["firstname"].' '.$_SESSION["lastname"].'</a></li>';
+	}
+		?>	
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="probootstrap-footer-widget">
+                <h3>Contact Info</h3>
+                <ul class="probootstrap-contact-info">
+                  <li><i class="icon-location2"></i> <span>198 West 21th Street, Suite 721 New York NY 10016</span></li>
+                  <li><i class="icon-mail"></i><span>info@domain.com</span></li>
+                  <li><i class="icon-phone2"></i><span>+123 456 7890</span></li>
+                </ul>
+              </div>
+            </div>
+           
+          </div>
+          <!-- END row -->
+          
+        </div>
+
+        <div class="probootstrap-copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 text-left">
+                <p>&copy; 2017 <a href="https://probootstrap.com/">ProBootstrap:Enlight</a>. All Rights Reserved. Designed &amp; Developed with <i class="icon icon-heart"></i> by <a href="https://probootstrap.com/">ProBootstrap.com</a></p>
+              </div>
+              <div class="col-md-4 probootstrap-back-to-top">
+                <p><a href="#" class="js-backtotop">Back to top <i class="icon-arrow-long-up"></i></a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+    <!-- END wrapper -->
+    
+
+    <script src="js/scripts.min.js"></script>
+    <script src="js/main.min.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
