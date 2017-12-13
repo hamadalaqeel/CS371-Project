2c5d5e236090acbd2c4f19f40f8be327d5bca543
Alyousfi <32346287+Alyousfi@users.noreply.github.com>
2017-12-13 21:37:40 +0300

    Making a PHP Footer File

---
+++ footer.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ACM Footer</title>
+    <meta name="description" content="Free Bootstrap Theme by ProBootstrap.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+     <!-- THE FOOTER -->
+      <footer class="probootstrap-footer probootstrap-bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="probootstrap-footer-widget">
+                <h3>About ACM</h3>
+                <p>ACM, the Association for Computing Machinery ,is the world's largest educational and scientific society, uniting computing educators, researchers and professionals to inspire dialogue, share resources and address the field's challenges. </p>
+                <h3>Social</h3>
+                <ul class="probootstrap-footer-social">
+                  <li><a href="https://twitter.com/PSU_RUH"><i class="icon-twitter"></i></a></li>
+                  <li><a href="https://www.facebook.com/PrinceSultanUniversity/"><i class="icon-facebook"></i></a></li>
+                  <li><a href="https://www.instagram.com/psu_ruh/"><i class="icon-instagram"></i></a></li>
+                  <li><a href="https://www.youtube.com/user/PSUofficial"><i class="icon-youtube"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-md-push-1">
+              <div class="probootstrap-footer-widget">
+                <h3>Links</h3>
+                <ul>
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="conferences.php">Conferences</a></li>
+                  <li><a href="people.php">People</a></li>
+                  <li><a href="news.php">News</a></li>
+                  <li><a href="contact.php">Contact</a></li>
+    
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="probootstrap-footer-widget">
+                <h3>Contact Info</h3>
+                <ul class="probootstrap-contact-info">
+                  <li><i class="icon-location2"></i> <span>Rafha Street, Riyadh, Saudi Arabia</span></li>
+                  <li><i class="icon-mail"></i><span>psu-acm@psu.edu.sa</span></li>
+                  <li><i class="icon-phone2"></i><span>+966 11 4548489</span></li>
+                </ul>
+              </div>
+            </div>
+           
+          </div>
+          <!-- END row -->
+          
+        </div>
+
+        <div class="probootstrap-copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 text-left">
+                <p>© 2017 <a class="js-backtotop">PSU: ACM CHAPTER</a>. All Rights Reserved. Designed &amp; Developed with <i class="icon icon-heart"></i> by <a>Hamad &amp; Megren &amp; Ibrahim</a></p>
+              </div>
+              <div class="col-md-4 probootstrap-back-to-top">
+                <p><a href="#" class="js-backtotop">Back to top <i class="icon-arrow-long-up"></i></a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    <!-- END wrapper -->
+    
+
+    <script src="js/scripts.min.js"></script>
+    <script src="js/main.min.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
