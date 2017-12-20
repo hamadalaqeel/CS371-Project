49957e3d0c7637f6000a864a699a0d78cea5742e
mugren <mugren@172.20.10.2>
2017-12-20 13:24:08 +0300

    edit

--- edit.php
+++ edit.php
@@ -1,102 +1,67 @@
 <?php
+/*
 
+  EDIT.PHP
 
+  Allows user to edit specific entry in database
 
-
+ */
 
 // creates the edit record form
-
 // since this form is used multiple times in this file, I have made it a function that is easily reusable
 
-function renderForm($title, $date, $shot_desc,$long_desc,$pic1,$pic2,$pic3 ,$error){
+function renderForm($id, $title, $short_desc, $error) {
+    ?>
 
-?>
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
+    <html>
 
+        <head>
 
-<html>
+            <title>Edit Record</title>
 
-<head>
+        </head>
 
-<title>Edit News</title>
+        <body>
 
-</head>
-
-<body>
-
-<?php
-    include 'navbar.php';
-
+    <?php
 // if there are any errors, display them
 
-if ($error != '')
+    if ($error != '') {
 
-{
+        echo '<div style="padding:4px; border:1px solid red; color:red;">' . $error . '</div>';
+    }
+    ?>
 
-echo '<div style="padding:4px; border:1px solid red; color:red;">'.$error.'</div>';
 
+
+            <form action="" method="post">
+
+                <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+
+                <div>
+
+                    <p><strong>ID:</strong> <?php echo $id; ?></p>
+
+                    <strong>First Name: *</strong> <input type="text" name="title" value="<?php echo $title; ?>"/><br/>
+
+                    <strong>Last Name: *</strong> <input type="text" name="short_desc" value="<?php echo $short_desc; ?>"/><br/>
+
+                    <p>* Required</p>
+
+                    <input type="submit" name="submit" value="Submit">
+
+                </div>
+
+            </form>
+
+        </body>
+
+    </html>
+
+    <?php
 }
-
-?>
-
-
-
-<form action="" method="post">
-
-
-
-<div>
-
-<p><strong>Title:</strong> <?php echo $Title; ?></p>
-
-<strong>Title: *</strong> <input type="text" name="title" value="<?php echo $title; ?>"/><br/>
-
-<strong>Date: *</strong> <input type="text" name="date" value="<?php echo $date; ?>"/><br/>
-
-<strong>Short Description: *</strong> <textarea rows="4" maxlength="200" placeholder="200 characters only" id="shortD">
-                                            <?php echo $shot_desc; ?></textarea> 
-    
-<strong>Long Description: *</strong> <textarea rows="8" maxlength="1500" placeholder="1500 characters only" id="longD">
-                                            <?php echo $long_desc; ?></textarea>  
-    
-<strong>First Image: </strong> <input id="image1" type="image" alt="Login" src="<?php echo $pic1; ?>">   
-<strong>Second Image: </strong> <input id="image2" type="image" alt="Login" src="<?php echo $pic2; ?>">   
-<strong>Third Image: </strong> <input id="image3" type="image" alt="Login" src="<?php echo $pic3; ?>">   
-<strong>Fourth Image: </strong> <input id="image4" type="image" alt="Login" src="<?php echo $pic4; ?>">       
-
-<p>* Required</p>
-
-<input type="submit" name="submit" value="Submit">
-
-</div>
-
-</form>
-
-    !-- THE FOOTER -->
-      <?php
-      include 'footer.php';
-      ?>
-      <!-- THE END OF THE FOOTER -->
-
-    <!-- END wrapper -->
-   
-
-    <script src="js/scripts.min.js"></script>
-    <script src="js/main.min.js"></script>
-    <script src="js/custom.js"></script>
-</body>
-
-</html>
-
-<?php
-
-}
-
-
-
-
-
-
 
 // connect to the database
 
@@ -106,156 +71,108 @@ include('include/dbconfig.php');
 
 // check if the form has been submitted. If it has, process the form and save it to the database
 
-if (isset($_POST['submit']))
-
-{
+if (isset($_POST['submit'])) {
 
 // confirm that the 'id' value is a valid integer before getting the form data
 
-if (is_numeric($_POST['title']))
-
-{
+    if (is_numeric($_POST['id'])) {
 
 // get form data, making sure it is valid
 
-$id = $_POST['title'];
+        $id = $_POST['id'];
 
+        $title = $_POST['title'];
 
+        $short_desc = $_POST['short_desc'];
 
-$title = mysql_real_escape_string(htmlspecialchars($_POST['title']));
-$shortDesc = mysql_real_escape_string(htmlspecialchars($_POST['shortD']));
-$longDesc = mysql_real_escape_string(htmlspecialchars($_POST['longD']));    
-    
 
 
 // check that firstname/lastname fields are both filled in
 
-if ($title == '' || $shortDesc == ''|| $longDesc == '')
-
-{
+        if ($title == '' || $short_desc == '') {
 
 // generate error message
 
-$error = 'ERROR: Please fill in all required fields!';
+            $error = 'ERROR: Please fill in all required fields!';
 
 
 
 //error, display form
 
-renderForm($title, $date, $shot_desc,$long_desc,$pic1,$pic2,$pic3 ,$error);
-
-}
-
-else
-
-{
+            renderForm($id, $title, $short_desc, $error);
+        } else {
 
 // save the data to the database
 
-mysql_query("UPDATE news SET title='$title', date='$date',short_desc='$shot_desc',
-                    long_desc='$long_desc',pic1='$pic1',pic2='$pic2',pic3='$pic3',pic4='$pic4'  WHERE id='$id'")
+            
+                $sql = "UPDATE news SET title='$title', short_desc='$short_desc' WHERE id='$id'";
 
-or die(mysql_error());
-
+                $result = $conn->query($sql);
 
 
 // once saved, redirect back to the view page
 
-header("Location: view.php");
-
-}
-
-}
-
-else{
+            header("Location: news_table.php");
+        }
+    } else {
 
 // if the 'id' isn't valid, display an error
 
-echo 'Error!';
-
-}
-
-}
-
-else
+        echo 'Error!';
+    }
+} else {
 
 // if the form hasn't been submitted, get the data from the db and display the form
-
-{
 
 
 
 // get the 'id' value from the URL (if it exists), making sure that it is valid (checing that it is numeric/larger than 0)
 
-if (isset($_GET['title']) && is_numeric($_GET['title']) && $_GET['title'] > 0)
-
-{
+    if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
 
 // query db
 
-$id = $_GET['title'];
+        $id = $_GET['id'];
 
-$result = mysql_query("SELECT * FROM news WHERE title=$title")
+        
+                $sql = "SELECT * FROM news WHERE id=$id";
 
-or die(mysql_error());
+                $result = $conn->query($sql);
 
-$row = mysql_fetch_array($result);
+   
+
+        $row = $result->fetch_assoc();
 
 
 
 // check that the 'id' matches up with a row in the databse
 
-if($row)
-
-{
+        if ($row) {
 
 
 
 // get data from db
 
-//$firstname = $row['firstname'];
+            $title = $row['title'];
 
-//$lastname = $row['lastname'];
-$title = $row['title'];
-$date = $row['date'];
-$shortD = $row['short_desc'];
-$longD = $row['long_desc'];
-$pic1 = $row['pic1'];
-$pic2 = $row['pic2'];
-$pic3 = $row['pic3'];
-$pic4 = $row['pic4'];
+            $short_desc = $row['short_desc'];
 
-    
 
 
 // show form
 
-renderForm($title, $date, $shot_desc,$long_desc,$pic1,$pic2,$pic3 , '');
-
-}
-
-else
+            renderForm($id, $title, $short_desc, '');
+        } else {
 
 // if no match, display result
 
-{
-
-echo "No results!";
-
-}
-
-}
-
-else
+            echo "No results!";
+        }
+    } else {
 
 // if the 'id' in the URL isn't valid, or if there is no 'id' value, display an error
 
-{
-
-echo 'Error!';
-
+        echo 'Error!';
+    }
 }
-
-}
-
 ?>
