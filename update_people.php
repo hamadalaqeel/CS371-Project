650350770b7f2902edba6084e4ef33e7e6ddf1c2
hamadalaqeel <214110427@psu.edu.sa>
2017-12-22 17:25:32 +0300

    Create update_people.php

---
+++ update_people.php
@@ -0,0 +1,57 @@
+<?php
+
+if (isset($_POST['email'])) {
+
+    //getting the email of the user.
+    if (isset($_POST['column'])) {
+        $column = $_POST['column'];
+    }
+
+    if (isset($_POST['editval'])) {
+        $editval = $_POST['editval'];
+    }
+
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+    }
+
+    //Setting up the database and deleting the user from the database.
+    include 'include/dbconfig.php';
+    //    $conn->query("SET NAMES utf8");
+    $sql = "UPDATE `committees` SET `$column` = '$editval' WHERE `committees`.`email` = '$email'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo '<div class="alert alert-success">
+  <strong>Success!</strong> The member with email "' . $email . '" was ? successfully.
+                         </div>';
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+} else if (isset($_POST['id'])) {
+    //getting the email of the user.
+    if (isset($_POST['column'])) {
+        $column = $_POST['column'];
+    }
+
+    if (isset($_POST['editval'])) {
+        $editval = $_POST['editval'];
+    }
+
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+    }
+
+    //Setting up the database and deleting the user from the database.
+    include 'include/dbconfig.php';
+    //    $conn->query("SET NAMES utf8");
+    $sql = "UPDATE `committees` SET `$column` = '$editval' WHERE `committees`.`id` = '$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo '<div class="alert alert-success">
+  <strong>Success!</strong> The member with email "' . $id . '" was ? successfully.
+                         </div>';
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+}
+?>
